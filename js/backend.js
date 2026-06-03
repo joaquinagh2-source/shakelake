f830d441a89c4fe2ba574c7033a23e4385410eff
Your Name <you@example.com>
2026-06-03 15:06:23 -0600

    Trabajo backend

--- js/backend.js
+++ js/backend.js
@@ -1,14 +1,43 @@
-document.getElementById('contactForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    
+document.addEventListener("DOMContentLoaded", function () {
+
+    const form = document.getElementById('contactForm');
     const success = document.getElementById('success');
-    success.style.display = 'block';
-    
-    // Limpiar el formulario
-    this.reset();
-    
-    // Ocultar el mensaje después de 8 segundos
-    setTimeout(() => {
-        success.style.display = 'none';
-    }, 8000);
+
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+
+        // enviar correo con EmailJS
+        emailjs.sendForm(
+            'service_e8e3cbi',
+            'template_l3yngth',
+            this
+        )
+        .then(() => {
+
+            success.style.display = 'block';
+            success.style.color = 'green';
+            success.textContent = "Mensaje enviado correctamente";
+
+            form.reset();
+
+            setTimeout(() => {
+                success.style.display = 'none';
+            }, 8000);
+
+        })
+        .catch((error) => {
+
+            success.style.display = 'block';
+            success.style.color = 'red';
+            success.textContent = "Error al enviar el mensaje";
+
+            console.log("Error:", error);
+
+            setTimeout(() => {
+                success.style.display = 'none';
+            }, 8000);
+        });
+
+    });
+
 });
